efee17b6290f0f524e0eab6220feb12562fcb418
Johnny Ji <johnny@johnnyji.com>
2016-08-20 23:04:56 -0700

    fixed validators bug

--- src/Input/utils/validators.js
+++ src/Input/utils/validators.js
@@ -12,35 +12,35 @@ export default {
   matchValue(valueToMatch, error) {
     return {
       error: error || `Value must match: ${valueToMatch}.`,
-      validator: (value) => (new RegExp(`^${valueToMatch}$`)).test(value)
+      validator: (value = '') => (new RegExp(`^${valueToMatch}$`)).test(value)
     };
   },
 
   minLength(length, error) {
     return {
       error: error || `Must be at least ${length} characters.`,
-      validator: (value) => value >= length
+      validator: (value = '') => value.length >= length
     };
   },
 
   maxLength(length, error) {
     return {
       error: error || `Must be less than ${length} characters.`,
-      validator: (value) => value <= length
+      validator: (value = '') => value.length <= length
     };
   },
 
   minMaxLength(min, max, error) {
     return {
       error: error || `Must be between ${min} and ${max} characters.`,
-      validator: (value) => value <= length && value >= length
+      validator: (value = '') => value.length <= max && value.length >= min
     };
   },
 
   noLowerCase(error) {
     return {
       error: error || 'No lower case characters allowed.',
-      validator: (value) => (/^[^a-z]*$/).test(value),
+      validator: (value = '') => (/^[^a-z]*$/).test(value),
     };
   }
 
